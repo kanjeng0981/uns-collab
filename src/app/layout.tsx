91df809fad4421ecc2b96b20783be61416a270c3
kanjeng0981 <kudasuki925@gmail.com>
2026-03-16 16:13:40 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,11 @@
-'use client' // ← Tambahkan jika pake hook
-import './globals.css' // ← WAJIB ADA INI DI BARIS TERATAS!
+import './globals.css' // ← WAJIB ADA DI BARIS PERTAMA
 import type { Metadata } from 'next'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className="antialiased">
-        {children} {/* ⭐ WAJIB ADA INI */}
+      <body className={`antialiased`}>
+        {children}
       </body>
     </html>
   );
