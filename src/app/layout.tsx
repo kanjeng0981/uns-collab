f3b8ef9848421e8125531451a66777fde3f4f787
kanjeng0981 <kudasuki925@gmail.com>
2026-03-16 15:22:58 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
-import './globals.css'
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body className="antialiased">
-        {children}
-      </body>
-    </html>
-  );
-}
+// Di src/app/layout.tsx
+<nav>
+  {/* Logo */}
+  
+  {/* Menu Navigasi */}
+  <ul>
+    <li><a href="/">Home</a></li>
+    <li><Link href="/projects/create">Buat Project</Link></li>
+  </ul>
+</nav>
